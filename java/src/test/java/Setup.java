38e3c1def67d2a953f4e3620cc69f901d8d157a7
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2017-06-02 17:58:34 +0200

    Kata Supermarket - Java edition

---
+++ java/src/test/java/Setup.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Setup {
+
+  @Test
+  public void isGood() {
+    assertTrue(true);
+  }
+}
