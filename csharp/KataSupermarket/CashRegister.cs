869240c76f80c19db89408b6c4c1ad72e902d9d3
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-12-02 18:23:31 +0100

    Test: When I checkout 3 apples, the system charges 150 cents - Green

--- csharp/KataSupermarket/CashRegister.cs
+++ csharp/KataSupermarket/CashRegister.cs
@@ -2,14 +2,16 @@ namespace KataSupermarket
 {
     public class CashRegister
     {
+        private int _total;
+
         public void Scan(string product)
         {
-            
+            _total += 50;
         }
         
         public int Checkout()
         {
-            return 50;
+            return _total;
         }
     }
 }
