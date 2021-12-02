8c5e6c33c74d8e8569c94cb8e72d96134d2265e6
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-12-02 16:21:20 +0100

    Test: When I checkout an apple, the system charges 50 cents

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -9,10 +9,19 @@ namespace KataSupermarket.Tests
         {
         }
 
-        [Test]
-        public void ShouldPass()
+        [Test(Description = "When I checkout an apple, the system charges 50 cents")]
+        public void OneAppleCosts50Cents()
         {
-            Assert.Pass();
+            //ASSERT
+            var expected = 50;
+            CashRegister register = new CashRegister();
+            
+            //ACT
+            register.Scan("apple");
+            var actual = register.checkout();
+            
+            //ASSERT
+            Assert.AreEqual(expected, actual);
         }
     }
 }
