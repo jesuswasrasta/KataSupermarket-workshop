e7e91fc336468204f539f40fed6af4f7911c2c4e
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-06 22:44:05 +0100

    When I checkout 2 pear, the system charges 60 cents - GREEN

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -12,6 +12,7 @@ namespace KataSupermarket.Tests
         [TestCase("apple", 1, ExpectedResult = 50, Description = "When I checkout an apple, the system charges 50 cents")]
         [TestCase("apple", 3, ExpectedResult = 150, Description = "When I checkout 3 apples, the system charges 150 cents")]
         [TestCase("pear", 1, ExpectedResult = 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
+        [TestCase("pear", 2, ExpectedResult = 60, Description = "When I checkout 2 pears, the system charges 60 cents")]
         [TestCase("pineapple", 1, ExpectedResult = 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
         [TestCase("banana", 1, ExpectedResult = 60, Description = "When I checkout 1 banana, the system charges 60 cents")]
         public int CheckoutTestCase(string product, int quantity)
