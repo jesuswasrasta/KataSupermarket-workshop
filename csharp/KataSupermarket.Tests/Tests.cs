03e2b10c70ff58733abe2b61c43fab6478b8e81f
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-06 18:02:49 +0100

    Refactor: introduced ExpectedResult property
    This is for a more more expressive and concise test case

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -9,12 +9,12 @@ namespace KataSupermarket.Tests
         {
         }
 
-        [TestCase("apple", 1, 50, Description = "When I checkout an apple, the system charges 50 cents")]
-        [TestCase("apple", 3, 150, Description = "When I checkout 3 apples, the system charges 150 cents")]
-        [TestCase("pear", 1, 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
-        [TestCase("pineapple", 1, 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
-        [TestCase("banana", 1, 60, Description = "When I checkout 1 banana, the system charges 60 cents")]
-        public void CheckoutTestCase(string product, int quantity, int expected)
+        [TestCase("apple", 1, ExpectedResult = 50, Description = "When I checkout an apple, the system charges 50 cents")]
+        [TestCase("apple", 3, ExpectedResult = 150, Description = "When I checkout 3 apples, the system charges 150 cents")]
+        [TestCase("pear", 1, ExpectedResult = 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
+        [TestCase("pineapple", 1, ExpectedResult = 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
+        [TestCase("banana", 1, ExpectedResult = 60, Description = "When I checkout 1 banana, the system charges 60 cents")]
+        public int CheckoutTestCase(string product, int quantity)
         {
             var register = new CashRegister();
 
@@ -22,10 +22,8 @@ namespace KataSupermarket.Tests
             {
                 register.Scan(product);    
             }
-            
-            var actual = register.Checkout();
-            
-            Assert.AreEqual(expected, actual);
+
+            return register.Checkout();
         }
     }
 }
