3bb9f918599d1a934a15ee3d91a3c171d8f7bf4a
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-06 17:54:10 +0100

    Refactor: remove old tests replaced by test case

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -9,21 +9,6 @@ namespace KataSupermarket.Tests
         {
         }
 
-        [Test(Description = "When I checkout an apple, the system charges 50 cents")]
-        public void OneAppleCosts50Cents()
-        {
-            //ASSERT
-            var expected = 50;
-            CashRegister register = new CashRegister();
-
-            //ACT
-            register.Scan("apple");
-            var actual = register.Checkout();
-
-            //ASSERT
-            Assert.AreEqual(expected, actual);
-        }
-
         [Test(Description = "When I checkout 3 apples, the system charges 150 cents")]
         public void ThreeApplesCosts150Cents()
         {
@@ -41,48 +26,10 @@ namespace KataSupermarket.Tests
             Assert.AreEqual(expected, actual);
         }
 
-        [Test(Description = "When I checkout 1 pear, the system charges 30 cents")]
-        public void OnePearCosts30Cents()
-        {
-            const int expected = 30;
-            var register = new CashRegister();
-
-            register.Scan("pear");
-            var actual = register.Checkout();
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test(Description = "When I checkout 1 pineapple, the system charges 220 cents")]
-        public void OnePineappleCosts220Cents()
-        {
-            const int expected = 220;
-            var register = new CashRegister();
-
-            register.Scan("pineapple");
-            var actual = register.Checkout();
-
-            Assert.AreEqual(expected, actual);
-        }
-
-                
-        [Test(Description = "When I checkout 1 banana, the system charges 60 cents")]
-        public void OneBananaCosts60Cents()
-        {
-            const int expected = 60;
-            var register = new CashRegister();
-            
-            register.Scan("banana");
-            var actual = register.Checkout();
-            
-            Assert.AreEqual(expected, actual);
-        }
-        
-        
-        [TestCase("apple", 1, 50)]
-        [TestCase("pear", 1, 30)]
-        [TestCase("pineapple", 1, 220)]
-        [TestCase("banana", 1, 60)]
+        [TestCase("apple", 1, 50, Description = "When I checkout an apple, the system charges 50 cents")]
+        [TestCase("pear", 1, 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
+        [TestCase("pineapple", 1, 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
+        [TestCase("banana", 1, 60, Description = "When I checkout 1 banana, the system charges 60 cents")]
         public void CheckoutTestCase(string product, int quantity, int expected)
         {
             var register = new CashRegister();
