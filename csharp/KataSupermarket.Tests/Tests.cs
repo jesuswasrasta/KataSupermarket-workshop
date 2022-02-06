05c163ddd44ac3c4749310e11ca0998e5bf43f75
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-06 17:57:22 +0100

    My test was buggy... :D

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -9,24 +9,8 @@ namespace KataSupermarket.Tests
         {
         }
 
-        [Test(Description = "When I checkout 3 apples, the system charges 150 cents")]
-        public void ThreeApplesCosts150Cents()
-        {
-            //ASSERT
-            var expected = 150;
-            CashRegister register = new CashRegister();
-
-            //ACT
-            register.Scan("apple");
-            register.Scan("apple");
-            register.Scan("apple");
-            var actual = register.Checkout();
-
-            //ASSERT
-            Assert.AreEqual(expected, actual);
-        }
-
         [TestCase("apple", 1, 50, Description = "When I checkout an apple, the system charges 50 cents")]
+        [TestCase("apple", 3, 150, Description = "When I checkout 3 apples, the system charges 150 cents")]
         [TestCase("pear", 1, 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
         [TestCase("pineapple", 1, 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
         [TestCase("banana", 1, 60, Description = "When I checkout 1 banana, the system charges 60 cents")]
