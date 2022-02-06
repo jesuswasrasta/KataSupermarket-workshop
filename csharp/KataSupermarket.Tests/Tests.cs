8ca6a780f0bf66fabcb280d83dff556cbec873bb
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-06 22:49:31 +0100

    When I checkout 3 apples, the system charges 130 cents instead of 150 - RED

--- csharp/KataSupermarket.Tests/Tests.cs
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -11,6 +11,8 @@ namespace KataSupermarket.Tests
 
         [TestCase("apple", 1, ExpectedResult = 50, Description = "When I checkout an apple, the system charges 50 cents")]
         [TestCase("apple", 3, ExpectedResult = 150, Description = "When I checkout 3 apples, the system charges 150 cents")]
+        [TestCase("apple", 3, ExpectedResult = 130, Description = "Offer: When I checkout 3 apples, the system charges 130 cents instead of 150")]
+        
         [TestCase("pear", 1, ExpectedResult = 30, Description = "When I checkout 1 pear, the system charges 30 cents")]
         [TestCase("pear", 2, ExpectedResult = 60, Description = "When I checkout 2 pears, the system charges 60 cents")]
         [TestCase("pineapple", 1, ExpectedResult = 220, Description = "When I checkout 1 pineapple, the system charges 220 cents")]
