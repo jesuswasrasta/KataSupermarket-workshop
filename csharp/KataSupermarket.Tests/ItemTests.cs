d1031022b3a72f5b33e9e20ffc95b39ee3cee221
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-02-16 17:23:48 +0100

    Add Item abstraction

---
+++ csharp/KataSupermarket.Tests/ItemTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace KataSupermarket.Tests
+{
+    public class ItemTests
+    {
+        [Test]
+        public void BuildAnApple()
+        {
+            var expectedApple = new Item("apple");
+            var actualApple = new Item("apple");
+
+            Assert.AreEqual(expectedApple, actualApple);
+        }
+    }
+}
---
+++ csharp/KataSupermarket/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KataSupermarket
+{
+    public record Item
+    {
+        public string ItemName { get; }
+
+        public Item(string itemName)
+        {
+            ItemName = itemName;
+        }
+    }
+}
