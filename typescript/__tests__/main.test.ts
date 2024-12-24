a24fb4f96623c5ac054707a901236071d149a93d
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2024-12-24 12:11:45 +0100

    TS: Fix code committed by mistake

--- typescript/__tests__/main.test.ts
+++ typescript/__tests__/main.test.ts
@@ -1,12 +1,7 @@
-import { checkout, checkoutFruit, Fruit } from '../src/main';
+import { isTrue } from '../src/main';
 
-describe('minimal checkout', () => {
-  it('When I checkout an apple, the system charges 50 cents', () => {
-    const value = checkout(1);
-    expect(value).toEqual(50);
-  });
-  it('When I checkout 1 pear, the system charges 30 cents', () => {
-    const value = checkoutFruit(Fruit.PEAR, 1);
-    expect(value).toEqual(30);
+describe('Testing library works as expected', () => {
+  it('True is true', () => {
+    expect(isTrue()).toEqual(true);
   });
 });
