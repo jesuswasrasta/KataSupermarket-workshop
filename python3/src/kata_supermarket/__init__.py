92d919fcfe0443713dc831adfc7516be4cac1e2b
Stefano Marello <smarello89@gmail.com>
2025-07-22 10:26:32 +0200

    Add Python3 Kata setup

---
+++ python3/src/kata_supermarket/__init__.py
@@ -0,0 +1,3 @@
+"""Kata Supermarket - Python3 Edition"""
+
+__version__ = "0.1.0"
---
+++ python3/src/kata_supermarket/main.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Kata Supermarket - Python3 Edition")
+
+
+if __name__ == "__main__":
+    main()
